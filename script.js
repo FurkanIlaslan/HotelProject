2c7a0840aa282feb62fa9b69747160bdccfb49fe
Furkan <furkanilaslan@gmail.com>
2024-09-11 11:15:39 +0300

    birçok değişiklik yapıldı

--- script.js
+++ script.js
@@ -25,3 +25,51 @@ const swiper = new Swiper('.mySwiper', {
     },
   
   });
+
+
+// ! Counter Design
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id,start,end,duration){
+        let obj = document.getElementById(id);
+        let current = start;
+        let range = end - start;
+        let increment = end > start ? 1 : -1;
+        let step = Math.abs(Math.floor(duration/range));
+        let timer = setInterval(()=>{
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer)
+            }
+        },step)
+    }
+    counter("count1",0, 100, 3000)
+    counter("count2",0, 200, 3000)
+    counter("count3",0, 300, 3000)
+    counter("count4",0, 400, 3000)
+})
+
+
+// Our Partner
+var ourPartnerSwiper = new Swiper(".our-partner", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    breakpoints: {
+        '991': {
+            slidesPerView: 5,
+            spaceBetween: 10,
+        },
+        '767': {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        '320': {
+            slidesPerView: 2,
+            spaceBetween: 8,
+        },
+    },
+});
